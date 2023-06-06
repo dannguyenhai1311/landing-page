b24e1ddebbe4eb58ae1cbc2d7d30c43ab7d90f94
Nguyenhaidan <nguyenhaidan13112000@gmail.com>
2023-06-06 10:09:31 +0700

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import footerLogo from "./img/footer.png";
 const Footer = () => {
     return (
         <footer className=" w-[100%] h-full md:max-h-[156px] md:mt-[140px]  p-5 bg-[#F1F1F1] text-[#727272]">
-        <div className="mx-[9%] md:w-[41%] md:gap-x-5 md:flex flex-center items-center">
+        <div className="mx-[9%] sm:w-[50%] md:w-[50%] lg:w-[41%] md:gap-x-5 md:flex flex-center items-center">
           <img src={footerLogo} className="w-[180px] h-[58px]" alt="" />
           <div className="items-center p-5 text-xs">
             <p>47340 부산광역시 부산진구 엄광로 176</p>
